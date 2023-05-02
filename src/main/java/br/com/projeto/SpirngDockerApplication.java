e46fb01ef274f9acaeeee67513994f2cd3beee61
GABRIEL DA SILVEIRA FERRER <gabriel.sferrer@senacsp.edu.br>
2023-05-02 09:51:23 -0300

    Projeto SpringBoot Docker

---
+++ src/main/java/br/com/projeto/SpirngDockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngDockerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/projeto/controller/PrincipalController.java
@@ -0,0 +1,32 @@
+package br.com.projeto.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrincipalController {
+     
+	@GetMapping("/home")
+	public String home() {
+		return "Bem vindo a página Home";
+	}
+	
+	@GetMapping("/clientes")
+	public List<String> clientes(){
+		List<String> cli = new ArrayList<String>();
+		cli.add("Microsoft");
+		cli.add("Google");
+		cli.add("Samsung");
+		cli.add("Apple");
+		cli.add("Xiaomi");
+		return cli;
+	}
+	
+	@GetMapping("/contato")
+	public String contato() {
+		return "contato@spring-docker.com.br";
+	}
+}
